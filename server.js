75fd4d37349618e3df985ef5a3e7b9590fd83b2e
Ethan <Ethanmbyrd@gmail.com>
2020-08-03 08:49:04 -0400

    adding in express router and profile api route

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const morgan = require ('morgan')
 const dotenv = require('dotenv')
 
 //Load env config 
-dotenv.config({path: './config.env'})
+dotenv.config({path: './config.env'});
+
 const app = express();
+
+//Profile Routes
+app.use('/api/v1/profile', require('./routes/profile'));
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}!`)
-})
+});
 
