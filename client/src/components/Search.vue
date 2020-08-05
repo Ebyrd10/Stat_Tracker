2100132ced68aea9a5de1d0a43bad3c2494f94b9
Ethan <Ethanmbyrd@gmail.com>
2020-08-04 21:00:40 -0400

    add search functionality, alerts, and validation

--- client/src/components/Search.vue
+++ client/src/components/Search.vue
@@ -36,6 +36,19 @@ export default {
     //when the search bar is active, eg. on home screen, show the apex person image
     beforeCreate() {
         document.body.className = "body-bg-image";
+    },
+    methods: {
+        onSubmit() {
+            if(!this.gamertag) {
+                this.$toasted.show("Please enter a gamertag",{
+                    duration: 3000,
+                    icon: 'exclamation-circle'
+                });
+            } 
+            else {
+                this.$router.push(`/profile/${this.platform}/${this.gamertag}`);
+            }
+        }
     }
 };
 </script>
--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueToasted from 'vue-toasted';
 
 Vue.config.productionTip = false
+
+//load Vue Toasted with font awesome support
+Vue.use(VueToasted, {
+  iconPack: 'fontawesome'
+})
 
 new Vue({
   router,
