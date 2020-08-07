249dcbbe6812eb29a657ca3f5253a7eca15f8b10
Ethan <Ethanmbyrd@gmail.com>
2020-08-06 22:57:00 -0400

    adding some profile display data and styling

--- client/src/components/Profile.vue
+++ client/src/components/Profile.vue
@@ -1,5 +1,36 @@
 <template>
-  <div> Profile </div>
+  <section> 
+      <!-- Loading in Progression -->
+      <div v-if="loading">
+          <h3> Loading ... </h3>
+      </div>
+    <!-- Error Reported -->
+    <div v-if="error">
+        <h3> {{error}}</h3>
+        <router-link to ="/"> Go Back </router-link>
+    </div>
+
+    <!-- Profile Display -->
+    <div v-if="profileData" class="container">
+        <h1 class="gamertag">
+            <img :src="profileData.platformInfo.avatarUrl" alt="Avatar" class="platform-avatar"/>
+            {{profileData.platformInfo.platformUserId}}
+        </h1>
+        <div class="grid">
+            <!-- active legend image -->
+            <img :src="profileData.segments[1].metadata.imageUrl" alt="">
+        </div>
+        <div>
+            <ul>
+                <li>
+                    <h4>Selected Legend</h4>
+                    <p>{{profileData.metadata.activeLegendName}}</p>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+</section>
 </template>
 
 <script>
@@ -36,6 +67,77 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.container {
+  background: rgba(0, 0, 0, 0.5);
+  color: #fff;
+  max-width: 700px;
+  margin: 1rem auto;
+  padding: 2rem 1.5rem;
+  border-radius: 20px;
+}
+h1.gamertag {
+  font-size: 2rem;
+  background: rgba(0, 0, 0, 0.6);
+  padding: 0.3rem 0.5rem;
+  text-align: center;
+  border-radius: 20px;
+  margin-bottom: 3rem;
+  display: flex;
+  align-items: center;
+}
+a {
+  display: inline-block;
+  margin-top: 2rem;
+  border: #fff 2px solid;
+  padding: 0.5rem 0.8rem;
+}
+a:hover {
+  border: #ccc 2px solid;
+  color: #ccc;
+}
+.platform-avatar {
+  width: 40px;
+  margin-right: 0.7rem;
+}
+img {
+  width: 100%;
+}
+.grid {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 1rem;
+}
+li {
+  background: rgba(0, 0, 0, 0.6);
+  padding: 1rem;
+  margin-bottom: 0.7rem;
+  border-radius: 10px;
+}
+li p {
+  font-size: 2rem;
+}
+li:first-child p {
+  font-size: 1.5rem;
+}
+li span {
+  font-size: 1rem;
+  color: #ccc;
+}
+@media (max-width: 500px) {
+  h1 {
+    font-size: 1.5rem;
+    display: block;
+    text-align: center;
+  }
+  .platform-avatar {
+    display: none;
+  }
+  .grid {
+    grid-template-columns: 1fr;
+  }
+  li p {
+    font-size: 1.5rem;
+  }
+}
 </style>
