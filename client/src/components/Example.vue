98664e89a6ba69f97a9dc91894dd80b462dd171b
Ethan Byrd <Ethanmbyrd@gmail.com>
2020-10-27 14:28:20 -0400

    Adding Example component

---
+++ client/src/components/Example.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="example" >
+      <div class="exampleBox" v-tooltip="'Load Example Profile'" v-on:click="onClick">
+          <div class="exampleSymbol">
+              <img src="../assets/help-circle.svg" alt="Example Profile" id="exampleIcon">
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name : 'Example',
+    methods: {
+        onClick() {
+            this.$router.push(`/profile/apex/origin/Facilitatur`);
+        }
+    }
+}
+</script>
+
+<style>
+.example{
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+}
+.exampleBox{
+    border: 4px #fff solid;
+    background: rgba(2, 3, 7, 0.3);
+    margin-bottom: -17px;
+    border-bottom: 0px;
+}
+.exampleBox:hover{
+    cursor: pointer;
+}
+.exampleSymbol{
+    margin: 4px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
